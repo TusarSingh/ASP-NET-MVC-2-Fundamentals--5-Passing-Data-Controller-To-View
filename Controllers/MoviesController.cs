53fd7c994e90791f25016652296c47f3337d13f2
Tusar Singh <rts0941@gmail.com>
2023-10-10 15:03:34 +0530

    Passing the Data Controller To View

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Tarun" };
+
+            return View(movie);
+
+           }
+
+
+
+        public ActionResult Rain()
+        {
+            var mo = new Movie() { Name = "Tusar" };
+
+            ViewData["Yes"] = mo;
+
+            ViewBag.yes = mo;
+
+            //var viewResult = new ViewResult();
+            //viewResult.ViewData.Model
+
+            return View(mo);
+            //return View();
+
+        }
+
+    }
+}
